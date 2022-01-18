42c1e2d18bf829c89e5df48eb06fc5f6f27423d3
Matt Laine <matt@brain-fuel.co>
2022-01-18 14:55:01 -0500

    initial commit

---
+++ SingleBunnyTest/Program.fs
@@ -0,0 +1,18 @@
+﻿open SingleBunny.DeDuplicator
+open System
+open System.Text
+open System.Text.RegularExpressions
+
+module Program = let [<EntryPoint>] main _ =
+    let GuidString = Guid.NewGuid().ToString()
+    printfn "%s{GuidString}" |> ignore
+    //let UniqueString = "bar" + GuidString + "foo"
+    //let UniqueString = GuidString + "foo"
+    //let UniqueString = "foo"
+    //let UniqueString = "bar" + GuidString
+    //let UniqueString = "bar" + "foo"
+    let UniqueString = "foobar"
+    let UniqueStringToBytesAndBackAgain =
+        Encoding.UTF8.GetBytes(UniqueString)
+        |> Encoding.UTF8.GetString
+    0
---
+++ SingleBunnyTest/Tests.fs
@@ -0,0 +1,10 @@
+﻿module Tests
+
+open System
+open Xunit
+open System.Text
+
+[<Fact>]
+let ``This test should pass`` () =
+    let byteArray = Encoding.UTF8.GetBytes("foo")
+    Xunit.Assert.True(true)
